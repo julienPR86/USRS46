940278f92dd2721a91966033a488ff25a61283ba
JulienPR86 <julien.pr86@gmail.com>
2024-11-05 16:22:00 +0100

    add maison class

--- ExemplePOO.cs
+++ ExemplePOO.cs
@@ -2,6 +2,17 @@ using System;
 
 namespace ExemplePOO
 {
+    public class Maison
+    {
+        public string Adresse;
+        public float Superficie;
+        public int NbPieces;
+        public bool Jardin;
+
+        public Maison()
+        {
+        }
+    }
     public class Program
     {
         public static void Main()
