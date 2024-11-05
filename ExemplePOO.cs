005e8aafbce5cb86b2900d999d73da2da814f99b
JulienPR86 <julien.pr86@gmail.com>
2024-11-05 16:27:19 +0100

    add foreach for print stats of maison

--- ExemplePOO.cs
+++ ExemplePOO.cs
@@ -21,12 +21,19 @@ namespace ExemplePOO
     {
         static void Main(string[] args)
         {
-            Maison UneMaison = new Maison();
+            Maison UneMaison = new Maison("11 Rue des Chartreux, 69001 Lyon",58f,4,false);
+            Maison UneAutreMaison = new Maison("4 place Saint Louis, 22100 Dinan",86.5f,5,true);
+            Maison UneDerniereMaison = new Maison("26 Boulevard Claude Lorrin, 40100 Dax",25.2f,2,false);
 
-            Console.WriteLine("Adresse = {0}", UneMaison.Adresse);
-            Console.WriteLine("Superficie = {0}", UneMaison.Superficie);
-            Console.WriteLine("Nombre de pièces = {0}", UneMaison.NbPieces);
-            Console.WriteLine("Présence d'un jardin = {0}", UneMaison.Jardin);
+            Maison[] CatalogueMaisons = new Maison[] {UneMaison, UneAutreMaison, UneDerniereMaison};
+
+            foreach(Maison M  in CatalogueMaisons)
+            {
+                Console.WriteLine("Adresse = {0}\n", M.Adresse);
+                Console.WriteLine("Superficie = {0}m²\n", M.Superficie);
+                Console.WriteLine("Nombre de pièces = {0}\n", M.NbPieces);
+                Console.WriteLine("Présence d'un jardin = {0}", M.Jardin ? "Oui" : "Non");
+            }
         }
     }
 }
