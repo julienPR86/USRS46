7139cea385352b53e15177c3977e3e009a0e3712
JulienPR86 <julien.pr86@gmail.com>
2024-11-05 16:32:12 +0100

    commit tostring method in maison and modifie foreach loop

--- ExemplePOO.cs
+++ ExemplePOO.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Text.RegularExpressions;
 
 namespace ExemplePOO
 {
@@ -16,6 +17,28 @@ namespace ExemplePOO
             NbPieces = nbPieces;
             Jardin = jardin;
         }
+        public override string ToString()
+        {
+            string toString = String.Format("Adresse = {0}\n", this.Adresse);
+            toString += String.Format("Superficie = {0}m²\n", this.Superficie);
+            toString += String.Format("Nombre de pièces = {0}\n", this.NbPieces);
+            toString += String.Format("Présence d'un jardin = {0}\n", this.Jardin ? "Oui" : "Non");
+            toString += String.Format("> VALEUR = {0}$", this.EvaluationValeur());
+            return toString;
+        }
+
+        public float EvaluationValeur()
+        {
+            int facteur = 3000;
+
+            if (this.Jardin) { facteur += 500; }
+            if (this.NbPieces > 3) { facteur += 200; }
+
+            if (Regex.IsMatch(this.Adresse, @"\bParis\b")) { facteur += 7000; }
+            else if (Regex.IsMatch(this.Adresse, @"\bLyon\b")) { facteur += 2000; }
+
+            return this.Superficie * facteur ;
+        }
     }
     public class Program
     {
@@ -27,12 +50,9 @@ namespace ExemplePOO
 
             Maison[] CatalogueMaisons = new Maison[] {UneMaison, UneAutreMaison, UneDerniereMaison};
 
-            foreach(Maison M  in CatalogueMaisons)
+            foreach(Maison M in CatalogueMaisons)
             {
-                Console.WriteLine("Adresse = {0}\n", M.Adresse);
-                Console.WriteLine("Superficie = {0}m²\n", M.Superficie);
-                Console.WriteLine("Nombre de pièces = {0}\n", M.NbPieces);
-                Console.WriteLine("Présence d'un jardin = {0}", M.Jardin ? "Oui" : "Non");
+                Console.WriteLine(M+"\n");
             }
         }
     }
