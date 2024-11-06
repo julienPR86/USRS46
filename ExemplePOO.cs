f24fbea3989da6329ceed0c0dda3f7a588a77708
JulienPR86 <julien.pr86@gmail.com>
2024-11-06 13:29:02 +0100

    ajout pièce

--- ExemplePOO.cs
+++ ExemplePOO.cs
@@ -3,6 +3,23 @@ using System.Text.RegularExpressions;
 
 namespace ExemplePOO
 {
+    public class Piece
+    {
+        public int Superficie;
+        public string Nom;
+
+        public Piece(int superficie, string nom)
+        {
+            Superficie = superficie;
+            Nom = nom;
+        }
+        
+        public override string ToString()
+        {
+            string toString = String.Format("La pièce {0} fait {1}m²", this.Nom, this.Superficie);
+            return toString;
+        }
+    }
     public class Bien
     {
         public string Adresse;
