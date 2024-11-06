585bfc2c42275097b464bc1f9bafdc0392b874ef
JulienPR86 <julien.pr86@gmail.com>
2024-11-06 13:26:12 +0100

    little modifications : tabs

--- ExemplePOO.cs
+++ ExemplePOO.cs
@@ -67,30 +67,30 @@ namespace ExemplePOO
 
         public Terrain(string adresse, float superficie, int nbCotesClotures, bool riviere) : base(adresse,superficie)
         {
-        NbCotesClotures = nbCotesClotures;
-        Riviere = riviere;
+            NbCotesClotures = nbCotesClotures;
+            Riviere = riviere;
         }
 
         public override string ToString()
         {
-        string toString = String.Format("Nombre de cotés clorutés = {0}\n", this.NbCotesClotures);
-        toString += String.Format("Présence d'une rivière = {0}\n", this.Riviere ? "Oui" : "Non");
-        toString += String.Format("> VALEUR = {0}$\n", this.EvaluationValeur());
-        return toString;
+            string toString = String.Format("Nombre de cotés clorutés = {0}\n", this.NbCotesClotures);
+            toString += String.Format("Présence d'une rivière = {0}\n", this.Riviere ? "Oui" : "Non");
+            toString += String.Format("> VALEUR = {0}$\n", this.EvaluationValeur());
+            return toString;
         }
 
         public new float EvaluationValeur()
         {
-        int facteur = 3000;
+            int facteur = 3000;
 
-        if (this.Riviere) { facteur += 600; }
+            if (this.Riviere) { facteur += 600; }
 
-        facteur += 100 * this.NbCotesClotures; 
+            facteur += 100 * this.NbCotesClotures; 
 
-        if (Regex.IsMatch(this.Adresse, @"\bParis\b")) { facteur += 7000; }
-        else if (Regex.IsMatch(this.Adresse, @"\bLyon\b")) { facteur += 2000; }
+            if (Regex.IsMatch(this.Adresse, @"\bParis\b")) { facteur += 7000; }
+            else if (Regex.IsMatch(this.Adresse, @"\bLyon\b")) { facteur += 2000; }
 
-        return this.Superficie * facteur ;
+            return this.Superficie * facteur ;
         }
     }
     public class Program
